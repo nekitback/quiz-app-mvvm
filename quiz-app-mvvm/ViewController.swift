7aa12ebd300379332673c5f9cb4217fe89158202
nekitback <artamnikita@yandex.ru>
2023-01-07 11:18:36 +0300

    build: initialize project

---
+++ quiz-app-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz-app-mvvm
+//
+//  Created by Nikita Artamonov on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
