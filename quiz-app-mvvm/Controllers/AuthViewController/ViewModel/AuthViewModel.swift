08869826b1c05f939590d0609de3da028d47ce28
nekitback <artamnikita@yandex.ru>
2023-01-24 11:37:14 +0300

    feat(session): add session logic

--- quiz-app-mvvm/Controllers/AuthViewController/ViewModel/AuthViewModel.swift
+++ quiz-app-mvvm/Controllers/AuthViewController/ViewModel/AuthViewModel.swift
@@ -4,4 +4,5 @@ final class AuthViewModel {
     var authType = Bindable<AuthType>(.login)
     var authManager = Bindable<AuthManager>(AuthManager())
     var databaseManager = Bindable<DatabaseManager>(DatabaseManager())
+    var session = Bindable<Session>(Session())
 }
